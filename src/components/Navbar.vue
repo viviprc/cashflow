f01798bf245f8d8f16e06af2773cce13c3f10739
unknown <vivi_reyes_c@hotmail.com>
2020-11-15 19:11:06 -0300

    Cambios al sidebar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,86 +1,104 @@
 <template>
   <v-container fluid class="navbar">
-            <v-app-bar app dark color="blue darken-4">
-          <v-app-bar-nav-icon
-            @click.stop="drawer = !drawer"
-          ></v-app-bar-nav-icon>
-          <h4>CASHFLOW</h4></v-app-bar
-        >
-        <v-sheet
-          height="100%"
-          class="overflow-hidden"
-          style="position: relative"
-        >
-          <v-container class="fill-height">
-            <v-row align="center" justify="center"> </v-row>
-          </v-container>
+    <v-app-bar app dark color="blue darken-4">
+      <v-app-bar-nav-icon v-if="authenticated" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <h4>CASHFLOW</h4></v-app-bar
+    >
+    <v-sheet height="100%" class="overflow-hidden" style="position: relative">
+      <v-container class="fill-height">
+        <v-row align="center" justify="center"> </v-row>
+      </v-container>
 
-          <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-icon>mdi-account-circle</v-icon>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Usuario</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list v-if="authenticated" class="sideBar" dense>
+          <router-link to="/inventory">
             <v-list-item>
-              <v-list-item-avatar>
-                <v-icon>mdi-account-circle</v-icon>
-              </v-list-item-avatar>
+              <v-list-item-icon>
+                <v-icon>mdi-format-list-bulleted</v-icon>
+              </v-list-item-icon>
 
               <v-list-item-content>
-                <v-list-item-title>Usuario</v-list-item-title>
+                <v-list-item-title>Inventario</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
+          </router-link>
+          <router-link to="/sale">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-cart</v-icon>
+              </v-list-item-icon>
 
-            <v-divider></v-divider>
+              <v-list-item-content>
+                <v-list-item-title>Vender</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+          <router-link to="/sales_history">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-timetable</v-icon>
+              </v-list-item-icon>
 
-            <v-list dense>
-              <v-list-item v-for="item in items" :key="item.title" @click="goTo(item.to)" link>
-                <v-list-item-icon>
-                  <v-icon>{{ item.icon }}</v-icon>
-                </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Historial de ventas</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
 
-                <v-list-item-content>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-navigation-drawer>
-        </v-sheet>
+          <v-list-item @click="logout">
+            <v-list-item-icon>
+              <v-icon>mdi-account-arrow-right-outline</v-icon>
+            </v-list-item-icon>
 
+            <v-list-item-content>
+              <v-list-item-title>Cerrar sesión</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-sheet>
   </v-container>
 </template>
 <script>
-import firebase from 'firebase'
+import firebase from "firebase/app";
+import "firebase/auth";
 export default {
   name: "Navbar",
   data() {
     return {
       drawer: null,
-      items: [
-        { title: "Iniciar sesión", icon: "mdi-login-variant", to:'login'},
-        { title: "Inventario", icon: "mdi-format-list-bulleted", to:'inventory' },
-        { title: "Vender", icon: "mdi-cart", to:'sale' },
-        { title: "Historial de ventas", icon: "mdi-timetable", to:'sales_history' },
-        { title: "Cerrar sesión", icon: "mdi-account-arrow-right-outline", to:'logout' },
-      ],
+      authenticated: false
     };
   },
-  methods:{
-    goTo(path){
-      const fullpath = `/${path}`;
-      if(path == 'logout'){
-        this.logout()
-
-      }else if(this.$router.history.current.path !== fullpath){
-        this.$router.push(fullpath)
-      }
-    },
+  created(){
+    firebase.auth().onAuthStateChanged(
+      (currentUser)=>{this.authenticated = currentUser}
+    )
+  },
+  methods: {
     logout() {
       firebase
         .auth()
         .signOut()
         .then(() => this.$router.push("login"));
     },
-  }
+  },
 };
 </script>
 <style lang="scss">
-div.navbar{
+div.navbar {
   position: absolute;
   top: 0;
   bottom: 0;
@@ -88,5 +106,9 @@ div.navbar{
   left: 0;
   padding: 0;
 }
- 
+.sideBar {
+  a {
+    text-decoration: none;
+  }
+}
 </style>
