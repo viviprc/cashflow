2ec2de0393beaf9fe24f195b7e7fad3a7adba982
unknown <vivi_reyes_c@hotmail.com>
2020-11-24 19:37:05 -0300

    probando guardianes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import Users from '../store/users'
 
 Vue.use(VueRouter)
 
@@ -10,7 +11,10 @@ const routes = [{
     path: '/',
     redirect: 'Sale'
   },
-
+  {
+    path: '*',
+    redirect: 'Sale'
+  },
   {
     path: '/login',
     name: 'Login',
@@ -19,26 +23,30 @@ const routes = [{
   {
     path: '/sales_history/:id',
     name: 'Detail',
-    component: () => import(/* webpackChunkName: "sales_history_detail" */ '../views/Detail.vue'),
+    component: () => import( /* webpackChunkName: "sales_history_detail" */ '../views/Detail.vue'),
+    props: true,
     meta: {
-      login: true
+      admin: true,
+      seller: true
     }
 
   },
   {
     path: '/sales_history',
     name: 'Sales_history',
-    component: () => import(/* webpackChunkName: "sales_history" */ '../views/Sales_history.vue'),
+    component: () => import( /* webpackChunkName: "sales_history" */ '../views/Sales_history.vue'),
     meta: {
-      login: true
+      admin: true,
+      seller: true
     }
   },
   {
     path: '/inventory',
     name: 'Inventory',
-    component: () => import(/* webpackChunkName: "inventory" */ '../views/Inventory.vue'),
+    component: () => import( /* webpackChunkName: "inventory" */ '../views/Inventory.vue'),
     meta: {
-      login: true
+      admin: true,
+      seller: true
     }
   },
   {
@@ -46,7 +54,17 @@ const routes = [{
     name: 'Sale',
     component: () => import( /* webpackChunkName: "sale" */ '../views/Sale.vue'),
     meta: {
-      login: true
+      admin: true,
+      seller: true
+    }
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import( /* webpackChunkName: "admin" */ '../views/Admin.vue'),
+    meta: {
+      admin: true,
+      seller:true
     }
   }
 
@@ -60,12 +78,38 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   let user = firebase.auth().currentUser;
-  let authRequired = to.matched.some(route => route.meta.login);
+  const dbUser = Users.state.users.find(u => u.data.uid === user.uid);
+  // const dataUser = dbUser.data.role;
+
+  // const role = dbUser ? dbUser.data.role : ''
+  let authRequired = to.matched.some(route => route.meta.admin);
+  // let sellerPermission = to.matched.some(route => route.meta.seller);
+
+  console.log(dbUser)
   if (!user && authRequired) {
     next('/login');
+  } else if (dbUser && !authRequired) {
+    next('/sale');
   } else {
-    next();
+    next()
   }
 });
+
+// router.beforeEach((to, from, next) => {
+//   // let user = firebase.auth().currentUser;
+//   const dbUser = Users.state.users.find(u => u.data.uid === user.uid)
+//   const role = dbUser ? dbUser.data.role : ''
+//   // let authRequired = to.matched.some(route => route.meta.tolog);
+//   let sellerPermission = to.matched.some(route => route.meta.seller);
+
+//   if (role === seller && sellerPermission) {
+//     next('/sale');
+//   } else if (role === seller && !adminPermission) {
+//     next('/sale');
+//   } else {
+//     next()
+//   }
+// });
+
 
 export default router
